050c54da88a222d58630cc5e21f858cfab3a5db7
Nolan Brubaker <nolan.brubaker@rackspace.com>
2014-08-21 17:10:11 -0400

    (x) as a proper alias.

--- helga_shaun.py
+++ helga_shaun.py
@@ -3,7 +3,7 @@ import random
 from helga.plugins import command
 
 
-@command('shaun', help='SHAAAAAAAAUUUUN')
+@command('shaun', aliases=['x'], help='SHAAAAAAAAUUUUN')
 def shaun(client, channel, nick, message, cmd, args):
     """
     SHAAAAAAAAUUN
@@ -12,7 +12,3 @@ def shaun(client, channel, nick, message, cmd, args):
         return "snes9x"
     return 'SH{a}{u}N'.format(a='A' * random.randint(1, 10),
                               u='U' * random.randint(1, 3))
-
-@command('x', help='SHAAAAAAAAUUUUN')
-def x(client, channel, nick, message, cmd, args):
-    return shaun(client, channel, nick, message, cmd, args)
